9fd54cea219e718e1427778c57e6294e0da8f1f3
pedro <pedro.vasquez@acftechnologies.com>
2020-11-04 17:52:06 -0600

    adding new proy cot

---
+++ cotizador/cotizador/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <h1>
+     <Header titulo="Cotizador" />
+    </h1>
+  );
+}
+
+export default App;
---
+++ cotizador/cotizador/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = ({ titulo }) => {
+  return (
+            <header>
+                <h1>{titulo}</h1>
+            </header>
+
+  );
+}
+
+
+export default Header;
