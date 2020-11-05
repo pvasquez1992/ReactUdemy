7f393d1117ca5c9f084a915c9b7d3fab3073fa86
pedro <pedro.vasquez@acftechnologies.com>
2020-11-05 10:29:39 -0600

    Adding

--- cotizador/cotizador/src/App.js
+++ cotizador/cotizador/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+      max-width: 600px;
+      margin: 0 auto;
+`;
+
+const ContendorFormulario = styled.div`
+      background-color : #FFF;
+      padding: 3rem;
+
+`;
 
 function App() {
   return (
-    <h1>
-     <Header titulo="Cotizador" />
-    </h1>
+    <Contenedor>
+      <Header titulo="Cotizador de seguros" />
+      <ContendorFormulario>
+        <Formulario />
+      </ContendorFormulario>
+
+    </Contenedor>
   );
 }
 
