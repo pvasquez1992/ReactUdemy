bf0b7f6a3d3f3b5239e64ea0fd591f698d67b990
pvasquez1992 <pvasquez1992@outlook.com>
2020-11-08 20:47:13 -0600

    adding

--- ContextBebidas/bebidas/src/components/Formulario.js
+++ ContextBebidas/bebidas/src/components/Formulario.js
@@ -1,8 +1,42 @@
 import React, { Fragment } from 'react';
 
 const Formulario = () => {
-    return (  
-       <h1>Formulario aqui</h1>
+    return (
+
+        <form
+            className="col-12 "
+        >
+            <fieldset className="text-center">
+                <legend>Busca bebidas por Categoria o Ingredientes</legend>
+            </fieldset>
+            <div className="row mt-4"
+            >
+                <div className="col-md-4">
+                    <input
+                        name="nombre"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por ingredientes"
+                    />
+                </div>
+                <div className="col-md-4"> 
+                    <select
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">-- Selecciona Categoría --</option>
+                    </select>                
+                </div>
+                <div className="col-md-4"> 
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
+
     );
-} 
+}
 export default Formulario
